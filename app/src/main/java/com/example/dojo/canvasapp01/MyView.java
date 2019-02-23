3dd16df5ae5283644fb27882f19292172539148f
Riku0209 <rikugoto0209@icloud.com>
2019-02-23 15:37:16 +0900

    MyViewを追加

---
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -0,0 +1,15 @@
+package com.example.dojo.canvasapp01;
+
+import android.content.Context;
+import android.nfc.NfcAdapter;
+import android.view.View;
+
+public class MyView extends View {
+
+    public MyView (Context Con) {
+        super(Con);
+    }
+
+
+
+}
