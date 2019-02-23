f8306b87d99295864d70df3b8a1e76376f9e300e
Riku0209 <rikugoto0209@icloud.com>
2019-02-23 16:59:10 +0900

    円を描いた

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -3,15 +3,18 @@ package com.example.dojo.canvasapp01;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.nfc.NfcAdapter;
 import android.view.View;
 
 public class MyView extends View {
-
+    private Paint paint;
 
     // コンストラクタ　（初期設定）
   public  MyView (Context Con) {
         super(Con);
+
+        paint = new Paint();
     }
 
     // 絵画の処理
@@ -19,7 +22,15 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
 
       //　背景
-        canvas.drawColor(Color.RED);
+        canvas.drawColor(Color.CYAN);
+
+      //円
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(30);
+        canvas.drawCircle(500,400,100,paint);
+        paint.setColor(Color.RED);
+      canvas.drawCircle(500,300,100,paint);
     }
 
 
