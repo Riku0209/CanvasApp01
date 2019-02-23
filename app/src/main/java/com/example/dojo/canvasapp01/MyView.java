7473d4d350fcf25f4f5d206c0b2cabd492c6dac5
Riku0209 <rikugoto0209@icloud.com>
2019-02-23 17:38:45 +0900

    三角形（パス）を描いた

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.nfc.NfcAdapter;
 import android.view.View;
 
 public class MyView extends View {
     private Paint paint;
+    private Path path;
 
     // コンストラクタ　（初期設定）
   public  MyView (Context Con) {
         super(Con);
 
         paint = new Paint();
+        path = new Path();
     }
 
     // 絵画の処理
@@ -41,8 +44,23 @@ public class MyView extends View {
         paint.setColor(Color.BLACK);
         canvas.drawLine(0,0,canvas.getWidth(),canvas.getHeight(),paint);
         canvas.drawLine(canvas.getWidth(),0,0,canvas.getHeight(),paint);
+
         //三角形（パス）
-        
+        paint.setColor(Color.WHITE);
+        path.moveTo(500,500);
+        path.lineTo(700,900);
+        path.lineTo(400,800);
+        path.lineTo(500,500);
+        canvas.drawPath(path,paint);
+
+        path.reset();
+        paint.setColor(Color.BLUE);
+        path.moveTo(400,400);
+        path.lineTo(100,100);
+        path.lineTo(200,100);
+        path.lineTo(400,400);
+        canvas.drawPath(path,paint);
+
     }
 
 
