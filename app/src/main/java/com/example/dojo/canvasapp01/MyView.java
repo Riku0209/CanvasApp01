cacb9a58d595bed602a70e11e5b8b23bf650381e
Riku0209 <rikugoto0209@icloud.com>
2019-02-23 17:58:49 +0900

    国旗を作ろうとした

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -24,42 +24,55 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
 
-      //　背景
+        //　背景
         canvas.drawColor(Color.CYAN);
 
-      //円
+        //円
         paint.setColor(Color.YELLOW);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(30);
-        canvas.drawCircle(500,400,100,paint);
+        canvas.drawCircle(500, 400, 100, paint);
         paint.setColor(Color.RED);
-      canvas.drawCircle(500,300,100,paint);
+        canvas.drawCircle(500, 300, 100, paint);
 
-      //四角
+        //四角
         paint.setColor(Color.GREEN);
         paint.setStrokeWidth(60);
-        canvas.drawRect(400,800,600,1000,paint);
+        canvas.drawRect(400, 800, 600, 1000, paint);
 
         //線
         paint.setColor(Color.BLACK);
-        canvas.drawLine(0,0,canvas.getWidth(),canvas.getHeight(),paint);
-        canvas.drawLine(canvas.getWidth(),0,0,canvas.getHeight(),paint);
+        canvas.drawLine(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+        canvas.drawLine(canvas.getWidth(), 0, 0, canvas.getHeight(), paint);
 
         //三角形（パス）
         paint.setColor(Color.WHITE);
-        path.moveTo(500,500);
-        path.lineTo(700,900);
-        path.lineTo(400,800);
-        path.lineTo(500,500);
-        canvas.drawPath(path,paint);
+        path.moveTo(500, 500);
+        path.lineTo(700, 900);
+        path.lineTo(400, 800);
+        path.lineTo(500, 500);
+        canvas.drawPath(path, paint);
 
         path.reset();
         paint.setColor(Color.BLUE);
-        path.moveTo(400,400);
-        path.lineTo(100,100);
-        path.lineTo(200,100);
-        path.lineTo(400,400);
-        canvas.drawPath(path,paint);
+        path.moveTo(400, 400);
+        path.lineTo(100, 100);
+        path.lineTo(200, 100);
+        path.lineTo(400, 400);
+        canvas.drawPath(path, paint);
+
+        //四角
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(100000);
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+
+        //円
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(300);
+        canvas.drawCircle(500, 500, 100, paint);
+
+
 
     }
 
